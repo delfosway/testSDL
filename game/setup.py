2e85cfa7a9b8cd9f802d851b0d56f3c944c66213
Martin <delfosway@gmail.com>
2017-10-26 14:50:47 -0300

    Kanban y Minutas
    #Agregados por si las dudas las capturas de Trello y anotadas las minutas en .txt

---
+++ game/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import py2exe
+
+
+setup(
+    name='Planeswalker',
+    version='',
+    packages=['test', 'modelo', 'negocio'],
+    url='',
+    license='',
+    author='DelfosWay',
+    author_email='',
+    description=''
+)
+
